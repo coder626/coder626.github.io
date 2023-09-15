e84819386af02faedc241bfa3055111089cab52b
Kevin Lavelle <kevinglavelle@gmail.com>
2023-09-14 18:43:49 -0700

    GIthub Button link

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-const button = document.getElementById("GithubButton");
+const githubButton = document.getElementById("GithubButton");
 
 // Add a click event listener to the button
-button.addEventListener("click", function() {
-    
+githubButton.addEventListener("click", function() {
+
     // Define the URL you want to navigate to
     const url = "https://github.com/coder626"; // Replace with your desired URL
+
+    // Use window.location to navigate to the URL
+    window.location.href = url;
+});
+
+const linkedinButton = document.getElementById("LinkedinButton");
+
+// Add a click event listener to the button
+linkedinButton.addEventListener("click", function() {
+    
+    // Define the URL you want to navigate to
+    const url = "https://www.linkedin.com/in/kevin-lavelle-a89298115/"; // Replace with your desired URL
 
     // Use window.location to navigate to the URL
     window.location.href = url;
